b0dc51a9d37b8ca94c1a4f2f4bf611bf1268ff38
Moisés Cadima <moises.cadima@adevcom.cl>
2016-10-23 23:43:26 -0300

    Preparación base

---
+++ application/views/shared/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<!-- Cómo llamar a una hora de estilo-->
+	<link rel="stylesheet" href="<?= base_url('content/css/main.css')?>">
+</head>
+<body>
+	<h1>Header</h1>
+	<a href="<?= base_url('welcome/index')?>">Link</a>
+</body>
+</html>
